c324b9ae753b0a88d6dea444a159162766f23366
gabyportu <gabriela.portugal@ucb.edu.bo>
2023-09-28 01:37:04 -0400

    elf y service de getactivos

--- src/app/service/activos.service.ts
+++ src/app/service/activos.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ActivoFijo } from '../modelos/activofijo';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActivosService {
+  
+  constructor(private http: HttpClient) {}
 
-  constructor() {}
+  getActivos(): Observable<ActivoFijo[]>{
+    return this.http.get<any>('/api/v1/activos-fijos').pipe(
+      map(response => response.content)
+    )
+  }
 }
+
--- src/app/store/activos.repository.ts
+++ src/app/store/activos.repository.ts
@@ -1,6 +1,7 @@
 import { createStore, withProps } from '@ngneat/elf';
 import { withEntities, selectAllEntities, setEntities, addEntities, updateEntities, deleteEntities, withUIEntities, withActiveId, selectActiveEntity, setActiveId, withActiveIds, selectActiveEntities, toggleActiveIds } from '@ngneat/elf-entities';
 import { Injectable } from '@angular/core';
+import { v4 as uuid } from 'uuid';
 
 export interface ActivosState{
   id: string;
@@ -25,3 +26,14 @@ const store = createStore(
   withEntities<ActivosState>(),
   withProps<ActivosProps>({filter: 'ALL'}),
 );
+
+@Injectable({providedIn: 'root'})
+export class ActivosRepository{
+  activos$ = store.pipe(selectAllEntities());
+  getActivosProps(){
+    return store.pipe(selectAllEntities());
+  }
+  addActivos(activos: string, precio: number, descripcion: string, fecha: string, ciudad: string, calle: string, avenida: string, bloque: string, porcentaje: number, fechaRe: string){
+    store.update(addEntities({id: uuid(), nombreActivo: activos, precio: precio, descripcion: descripcion, fecha: fecha, ciudad: ciudad, calle: calle, avenida: avenida, bloque: bloque, porcentaje: porcentaje, fechaRe: Date.now().toString()}));
+  }
+}
